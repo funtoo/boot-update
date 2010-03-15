af1dbc46f375d60c61327637ea085e9a9514108b
Daniel Robbins <drobbins@funtoo.org>
2010-03-15 17:46:15 -0600

    automation fixes

--- make.sh
+++ make.sh
@@ -2,12 +2,18 @@
 
 VERSION=`cat VERSION`
 
-sdist() {
+prep() {
 	install -d dist
 	rm -f dist/coreboot-$VERSION*
 	cd doc
 	cat boot-update.8.rst | sed -e "s/##VERSION##/$VERSION/g" | rst2man.py > boot-update.8
 	cat boot.conf.5.rst | sed -e "s/##VERSION##/$VERSION/g" | rst2man.py > boot.conf.5
+	cd ..
+	sed -i -e '/^version=/s/^.*$/version="'$VERSION'"/g' sbin/boot-update 
+}
+
+commit() {
+	cd doc
 	git add boot-update.8 boot.conf.5
 	cd ..
 	git commit -a -m "$VERSION distribution release"
@@ -15,20 +21,24 @@ sdist() {
 	bzip2 dist/coreboot-$VERSION.tar
 }
 
-webupdate() {
+web() {
 	cp dist/coreboot-$VERSION.tar.bz2 /root/git/website/archive/coreboot
 	cd /root/git/website && git add archive/coreboot/* && git commit -a -m "new coreboot"
 	./install.sh
 }
 
-if [ "$1" = "sdist" ]
+if [ "$1" = "prep" ]
 then
-	sdist
-elif [ "$1" = "webupdate" ]
+	prep
+elif [ "$1" = "commit" ]
 then
-	webupdate
+	commit
+elif [ "$1" = "web" ]
+then
+	web
 elif [ "$1" = "all" ]
 then
-	sdist
-	webupdate
+	prep
+	commit
+	web
 fi
