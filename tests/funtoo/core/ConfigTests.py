b32b95c5fa5f4d8bdcf1fd480b48bed34f05e87f
Brant Gurganus <brant@gurganus.name>
2010-10-20 13:45:54 -0400

    started ConfigTests

---
+++ tests/funtoo/core/ConfigTests.py
@@ -0,0 +1,19 @@
+# -*- coding: ascii; tab-width: 4; indent-tabs-mode: nil -*-
+import unittest
+from funtoo.core import config
+
+class ErrorTests(unittest.TestCase):
+    def test_noarg(self):
+        error = config.ConfigFileError()
+        expected = "(no message)"
+        self.assertEqual(expected, str(error))
+
+    def test_onearg(self):
+        error = config.ConfigFileError('test')
+        expected = "test"
+        self.assertEqual(expected, str(error))
+
+    def test_twoargs(self):
+        error = config.ConfigFileError('test1', 'test2')
+        expected = "(no message)"
+        self.assertEqual(expected, str(error))
