00c0896e6689ad2109c502d04591e5dfcbaba6d1
Daniel Robbins <drobbins@funtoo.org>
2010-10-28 00:05:25 -0600

    1.4.10 distribution release

--- make.sh
+++ make.sh
@@ -17,6 +17,7 @@ commit() {
 	git add boot-update.8 boot.conf.5
 	cd ..
 	git commit -a -m "$VERSION distribution release"
+	git tag "$VERSION"
 	git archive --format=tar --prefix=boot-update-${VERSION}/ HEAD > dist/boot-update-${VERSION}.tar
 	bzip2 dist/boot-update-$VERSION.tar
 }
