9e020608a955b0cbeda5aab72b586bf3257be4a6
Brant Gurganus <brant@gurganus.name>
2010-11-01 18:19:15 -0400

    I added documentation for the funtoo.boot package.

--- python/modules/funtoo/boot/__init__.py
+++ python/modules/funtoo/boot/__init__.py
@@ -1,2 +1,5 @@
 # -*- coding: ascii -*-
+""" The funtoo.boot package provides mechanisms for interacting with the boot
+configuration as well as a means for interacting with the configuration
+mechanisms of various bootloaders. """
 __all__ = [ "config" ]
