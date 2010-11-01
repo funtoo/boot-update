c5570c66719efd38d37894a291a231640a0e7f13
Brant Gurganus <brant@gurganus.name>
2010-11-01 18:03:39 -0400

    modified test script to run with and without coverage

--- runtests.sh
+++ runtests.sh
@@ -1,12 +1,12 @@
 pushd "`dirname $0`" > /dev/null
 cd tests
-#if [ "$1" == "coverage" ]; then
-#	PYTHONPATH="`pwd`/../python/modules" coverage run --source ../python/modules,../sbin --branch FuntooSuite.py;
-#	coverage report -m;
-#	coverage html;
-#else
+if [ "$1" == "coverage" ]; then
+	PYTHONPATH="`pwd`/../python/modules" coverage run --source ../python/modules,../sbin --branch FuntooSuite.py;
+	coverage report -m;
+	coverage html;
+else
 	PYTHONPATH="`pwd`/../python/modules" python FuntooSuite.py;
-#fi
+fi
 exitcode=$?
 popd > /dev/null
 exit $exitcode
