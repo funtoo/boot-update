a1a140f31a030ee93b1ccaf15cea8062be71af6e
Brant Gurganus <brant@gurganus.name>
2010-11-01 18:28:38 -0400

    committing a test that isn't committed

---
+++ tests/funtoo/boot/extensions/GrubTests.py
@@ -0,0 +1,10 @@
+# -*- coding: ascii; tab-width: 4; indent-tabs-mode: nil -*-
+import unittest
+from funtoo.boot.extensions import grub
+from funtoo.boot import config
+
+class GrubTests(unittest.TestCase):
+    def test_missing_defname(self):
+        ext = grub.GRUBExtension(config.DefaultBootConfigFile(), True)
+        ext.defname
+        ext.defpos
